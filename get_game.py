de61d1ecf4437485954c18e41c36ffae2a14991d
Eric Karschner <erickarschner@gmail.com>
2024-10-06 13:11:48 -0400

    Fix poor handling of empty clues

--- get_game.py
+++ get_game.py
@@ -23,24 +23,15 @@ def get_game_with_id(game_id):
         title = r.select_one('h2').text
         if default_round:
             smallest_clue_value = int(r.select_one('td.clue_value').text[1:])
-            rounds.append(pull_default_from_table(default_round, name=title, round_multiplier=smallest_clue_value // 100))
+            rounds.append(pull_default_from_table(default_round, name=title, round_multiplier=smallest_clue_value // 100, default_max_wager=smallest_clue_value*100))
         final_round = r.select_one('table.final_round')
         if final_round:
-            rounds.append(pull_final_from_table(final_round, name=title))
+            rounds.append(pull_final_from_table(final_round, name=title, default_max_wager=3000))
 
     return {'rounds': rounds}
 
-def get_default_max_wager_for_round(round_name):
-    if round_name == 'Jeopardy':
-        return 1000
-    if round_name == 'Double Jeopardy':
-        return 2000
-    if round_name == 'Final Jeopardy':
-        return 3000
-    return 1000
 
-
-def pull_default_from_table(table, name, round_multiplier=2):
+def pull_default_from_table(table, name, round_multiplier, default_max_wager):
     categories = [{'category': td.text, 'clues': []} for td in table.select("td.category_name")]
 
     clue_rows = table.find_all("tr", recursive=False)[1:] # The first row is categories
@@ -48,17 +39,19 @@ def pull_default_from_table(table, name, round_multiplier=2):
         clueEls = tr.select("td.clue")
         for i, td in enumerate(clueEls):
             cost = 100 * (row_i + 1) * round_multiplier
-            clue = td.select_one("td.clue_text").text or "This clue was missing"
-            response = td.select_one("em.correct_response").text or "This response was missing"
+            clue_td = td.select_one("td.clue_text")
+            clue = clue_td.text if clue_td else "This clue was missing"
+            response_td = td.select_one('em.correct_response')
+            response = response_td.text if response_td else "This response was missing"
             is_daily_double = td.select_one("td.clue_value_daily_double") is not None
             categories[i]['clues'].append({'clue': clue, 'response': response, 'cost': cost, 'is_daily_double': is_daily_double})
-    return {'categories': categories, 'name': name, 'round_type': 'DefaultRound', 'default_max_wager': get_default_max_wager_for_round(name)}
+    return {'categories': categories, 'name': name, 'round_type': 'DefaultRound', 'default_max_wager': default_max_wager}
 
-def pull_final_from_table(table, name):
+def pull_final_from_table(table, name, default_max_wager):
     category = table.select_one('td.category_name').text
     clue = table.select_one("td#clue_FJ").text
     response = table.select_one('em.correct_response').text
-    return {'category': category, 'clue': clue, 'response': response, 'round_type': 'FinalRound', 'name': 'Final Jeopardy', 'default_max_wager': get_default_max_wager_for_round('Final Jeopardy')}
+    return {'category': category, 'clue': clue, 'response': response, 'round_type': 'FinalRound', 'name': 'Final Jeopardy', 'default_max_wager': default_max_wager}
 
 
 if __name__ == '__main__':
